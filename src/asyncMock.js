cfefa8b6d76ddc967734f90c0bed727bfbb76d46
gamiralles <gasti.miralles@gmail.com>
2023-11-15 12:04:02 -0300

    Second Commit

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -1,81 +1,111 @@
 const post = [
-    {
-        id:'2',
-        category:'moreno',
-        img:'https://images.pexels.com/photos/11278486/pexels-photo-11278486.jpeg?auto=compress&cs=tinysrgb&w=400',
-        titulo:'Glaciar Perito Moreno',
-        info:'El Glaciar Perito Moreno es uno de los principales atractivos turísticos de la Patagonia argentina. Se encuentra en el Parque Nacional Los Glaciares, en la provincia de Santa Cruz. Es uno de los glaciares más grandes del mundo, con una superficie de 250 kilómetros cuadrados.',
-        titulo2:'Puntos Importantes:',
-        info2a:'El glaciar se encuentra en constante movimiento, lo que lo hace un espectáculo natural único.',
-        info2b:'Se pueden realizar diferentes excursiones para apreciar el glaciar, como caminatas, navegaciones y minitrekking.',
-        info2c:'El mejor momento para visitar el glaciar es durante los meses de verano, cuando las temperaturas son más altas y el clima es más estable.',
-        titulo3:'Lugares a tener en cuenta:',
-        info3a:'Mirador del Glaciar Perito Moreno: este mirador ofrece una vista panorámica del glaciar.',
-        info3b:'Sendero de los Nogales: este sendero de 2 kilómetros lleva a los visitantes al frente del glaciar.',
-        info3c:'Glaciares Gourmet Boat Trip: esta excursión en barco permite a los visitantes acercarse al glaciar y observarlo desde diferentes ángulos.',
-        titulo4:'Excursiones para hacer:',
-        info4a:'Minitrekking: esta excursión permite a los visitantes caminar sobre el glaciar',
-        info4b:'Navegation: esta excursión en barco permite a los visitantes ver el glaciar desde el agua.',
-        info4c:'',
-    },
-    {
-        id:'3',
-        category:'chalten',
-        img:'https://images.pexels.com/photos/3428177/pexels-photo-3428177.jpeg?auto=compress&cs=tinysrgb&w=400',
-        titulo:'El Chalten',
-        info:'El Chalten es un pueblo ubicado en el Parque Nacional Los Glaciares, en la provincia de Santa Cruz. Es un destino popular para los amantes del trekking, el montañismo y la naturaleza.',
-        titulo2:'Puntos Importantes:',
-        info2a:'El Chalten es la base para explorar el Cerro Fitz Roy, una de las montañas más emblemáticas de la Patagonia.',
-        info2b:'El pueblo también ofrece una variedad de otras actividades, como caminatas, pesca, kayak y rafting.',
-        info2c:'El mejor momento para visitar El Chalten es durante los meses de verano, cuando las temperaturas son más altas y el clima es más estable.',
-        titulo3:'Lugares a tener en cuenta:',
-        info3a:'Cerro Fitz Roy: esta montaña de 3.405 metros de altura es una de las más desafiantes y gratificantes de escalar en la Patagonia.',
-        info3b:'Laguna de Los Tres: esta laguna de aguas turquesas se encuentra al pie del Cerro Fitz Roy.',
-        info3c:'Glaciar Torre: este glaciar de color azul intenso es otro de los atractivos de El Chalten.',
-        titulo4:'Excursiones para hacer:',
-        info4a:'Hiking Laguna de Los Tres: esta caminata de 7 kilómetros lleva a los visitantes a la laguna de Los Tres.',
-        info4b:'Hiking Laguna Torre: esta caminata de 12 kilómetros lleva a los visitantes al glaciar Torre.',
-        info4c:'',
-    },
-    {
-      id:'4',
-      category:'austral',
-      img:'https://images.pexels.com/photos/14437116/pexels-photo-14437116.jpeg?auto=compress&cs=tinysrgb&w=400',
-      titulo:'Carretera Austral',
-      info:'La Carretera Austral es una ruta de 1.240 kilómetros que recorre la Patagonia chilena. Es una de las rutas más pintorescas del mundo, ya que atraviesa bosques, montañas, glaciares y lagos.',
-      titulo2:'Puntos Importantes:',
-      info2a:'La Carretera Austral comienza en Puerto Montt y termina en Villa O`Higgins.',
-      info2b:'La ruta es accesible en automóvil, pero también se puede recorrer en bicicleta, autobús o incluso a pie',
-      info2c:'El mejor momento para viajar por la Carretera Austral es durante los meses de verano, cuando las temperaturas son más altas y el clima es más estable.',
-      titulo3:'Lugares a tener en cuenta:',
-      info3a:'Parque Nacional Torres del Paine: este parque nacional alberga algunas de las montañas más emblemáticas de la Patagonia, como las Torres del Paine.',
-      info3b:'Parque Nacional Vicente Pérez Rosales: este parque nacional cuenta con una gran variedad de paisajes, como bosques, montañas, lagos y glaciares.',
-      info3c:'Parque Nacional Los Glaciares: este parque nacional alberga el glaciar Perito Moreno, uno de los más grandes del mundo.',
-      titulo4:'Excursiones para hacer:',
-      info4a:'Hiking Torres del Paine: esta caminata de 5 días lleva a los visitantes a las Torres del Paine.',
-      info4b:'Crucero por el Lago Argentino: esta excursión permite a los visitantes ver el glaciar Perito Moreno desde el agua.',
-      info4c:'Pesca en el río Futaleufú: este río es conocido por su pesca de trucha.',
-    }
-]
+  {
+    id: "2",
+    category: "moreno",
+    img: "https://images.pexels.com/photos/11278486/pexels-photo-11278486.jpeg?auto=compress&cs=tinysrgb&w=400",
+    titulo: "Glaciar Perito Moreno",
+    info: "El Glaciar Perito Moreno es uno de los principales atractivos turísticos de la Patagonia Argentina. Se encuentra en el Parque Nacional Los Glaciares, en la provincia de Santa Cruz. Es uno de los glaciares más grandes del mundo, con una superficie de 250 kilómetros cuadrados.",
+    titulo2: "Puntos Importantes",
+    info2a:
+      "Se encuentra a solo 80 kilómetros de la ciudad de El Calafate, lo que lo hace un destino turístico muy popular.",
+    info2b:
+      "Avanza a una velocidad de 2 metros por día, lo que genera un fenómeno único en el mundo: el desprendimiento de enormes bloques de hielo, que caen al lago Argentino con un estruendo atronador.",
+    info2c:
+      "El mejor momento para visitarlo es durante los meses de verano, cuando las temperaturas son más altas y el clima es mas estable.",
+    titulo3: "Que hacer?:",
+    info3a:
+      "Recorrido por las Pasarelas. Adentrate en el bosque permitiendo vivir la magia del Glaciar desde una perspectiva única con unas excelentes vistas Panorámicas.",
+    info3b:
+      "Minitrekking por el Glaciar. Camina sobre el hielo experimentando la fuerza y el poder de la Naturaleza.",
+    info3c:
+      "Navegación por Todos los Glaciares. Disfruta de un paseo en barco por el Lago Argentino, observa de cerca los glaciares Perito Moreno, Upsala, Spegazzini y otros, disfrutando de las vistas panorámicas de la Patagonia.",
+    info3d:
+      "Safari Azul, navega en busca de fauna autóctona observando pingüinos lobos marinos aves y otros animales. Disfruta de un espectáculo natural Inolvidable.",
+    info3e:
+      "Kayak frente al Glaciar. Rema entre los icebergs que se desprenden del Glaciar, sintiendo la emoción de estar rodeado de hielo, una experiencia única en la Patagonia.",
+    titulo4: "Nuestro Servicio:",
+    info4a: "Traslado al Parque Nacional",
+    info4b: "Traslado al Puerto Punta Bandera",
+    info4c: "Traslado al Puerto Bajo las Sombras",
+  },
+  {
+    id: "3",
+    category: "chalten",
+    img: "https://images.pexels.com/photos/3428177/pexels-photo-3428177.jpeg?auto=compress&cs=tinysrgb&w=400",
+    titulo: "El Chalten",
+    info: "El Chalten es un pueblo ubicado al Norte del Parque Nacional Los Glaciares, en la provincia de Santa Cruz. Es un destino popular para los amantes del trekking, el montañismo y la naturaleza.",
+    titulo2: "Puntos Importantes:",
+    info2a:
+      "El Chalten es la base para explorar el Cerro Fitz Roy, una de las montañas más emblemáticas de la Patagonia.",
+    info2b:
+      "El pueblo también ofrece una variedad de otras actividades, como caminatas, pesca, kayak y rafting.",
+    info2c:
+      "El mejor momento para visitar El Chalten es durante los meses de verano, cuando las temperaturas son más altas y el clima es mas estable.",
+    titulo3: "Que Hacer?",
+    info3a:
+      "A 12,5 kilómetros de El Chalten, se encuentra la Laguna de los Tres, uno de los trekkings más populares de la Patagonia. Con una dificultad media, el sendero ofrece vistas privilegiadas del imponente Cerro Fitz Roy, una de las cumbres más emblemáticas de la región.",
+    info3b:
+      "A 9,5 kilómetros de El Chalten, se encuentra la Laguna Torre, otro de los trekkings más populares de la región. Con una dificultad media, el sendero ofrece vistas de la base del Cerro Torre, una de las montañas más desafiantes del mundo.",
+    info3c:
+      "A 4 kilómetros de El Chalten, se encuentra la Laguna Capri, un tranquilo lugar ideal para relajarse y disfrutar de la naturaleza. Con una dificultad baja, el sendero ofrece la vista de una laguna de aguas azules rodeada de bosque nativo.",
+    info3d:
+      "A 3,5 kilómetros de El Chalten, se encuentra el Chorrillo del Salto, una hermosa cascada de deshielo. Con una dificultad baja, el sendero ofrece vistas de la cascada y el bosque nativo.",
+    info3e:
+      "A 37 kilómetros de El Chalten, se encuentra el Lago del Desierto, un imponente espejo de agua cristalina rodeado de un paisaje natural de cerros, montañas y bosque",
+    titulo4: "Nuestro Servicio",
+    info4a: "Full Day Chalten, ida y vuelta el día saliendo desde El Calafate",
+    info4b: "Traslado Aeropuerto FTE - El Chalten",
+    info4c: "Traslado EL Calafate - El Chalten",
+  },
+  {
+    id: "4",
+    category: "austral",
+    img: "https://images.pexels.com/photos/14437116/pexels-photo-14437116.jpeg?auto=compress&cs=tinysrgb&w=400",
+    titulo: "Carretera Austral",
+    info: "La Carretera Austral es una ruta de 1.240 kilómetros que recorre la Patagonia Chilena. Es una de las rutas más pintorescas del mundo, ya que atraviesa bosques, montañas, glaciares y lagos.",
+    titulo2: "Puntos Importantes:",
+    info2a:
+      "Inicia su recorrido en la ciudad de El Calafate y finaliza en la ciudad de Bariloche.",
+    info2b:
+      "Recorre la emblemática Ruta Nacional 40 de Argentina y la ruta austral 7 de la Patagonia Chilena.",
+    info2c:
+      "El mejor momento para hacerlo es durante los meses de verano, cuando las temperaturas son más altas y el clima es mas estable.",
+    titulo3: "Que hacer?:",
+    info3a:
+      "Sendero de Colores en el PN Patagonia, disfruta de un paseo por un sendero de colores experimentando la belleza natural de la Patagonia. Una caminata ideal para todas las edades.",
+    info3b:
+      "Capillas de Mármol, visita una de las maravillas naturales más icónicas de la Patagonia, conoce sus formaciones rocosas de mármol esculpidas por el agua y el viento durante millones de años. Una navegación perfecta para los amantes de la naturaleza y la fotografía.",
+    info3c:
+      "Cabalgata por el Cerro Castillo, recorre los senderos del PN, experimenta la Patagonia de una manera única, con vistas panorámicas de los picos nevados y los bosques nativos.",
+    info3d:
+      "PN Queulat y Baños en termas naturales, visita el PN y descubrí sus glaciares, bosques y cascadas. Relajate en las termas naturales de Puyuhuapi, perfecto para descansar y disfrutar de la Naturaleza.",
+    info3e:
+      "Rafting por el rio Futaleufu, vivi la emoción en uno de los ríos más rápidos de America del Sur, desafía las aguas bravas y experimenta la adrenalina de la naturaleza, una actividad ideal para los amantes de la aventura.",
+    titulo4: "Nuestro Servicio:",
+    info4a: "Recorrido de 6 días y 5 noches.",
+    info4b: "Traslados y logísticas, a lo largo de todo el recorrido.",
+    info4c: "Reservas de alojamiento y excursiones.",
+  },
+];
 
 const inicio = [
-    {
-        img:'https://images.pexels.com/photos/17897657/pexels-photo-17897657/free-photo-of-resfriado-glaciar-iceberg-paisaje.jpeg?auto=compress&cs=tinysrgb&w=400'
-    }
-]
+  {
+    img: "https://images.pexels.com/photos/17897657/pexels-photo-17897657/free-photo-of-resfriado-glaciar-iceberg-paisaje.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+];
 
 export const getInicio = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(inicio);
-      }, 1000);
-    });
-  };
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(inicio);
+    }, 1000);
+  });
+};
 
 export const getPostByCategory = (categoryId) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(post.filter((pst) => pst.category === categoryId));
-      }, 1000);
-    });
-  };
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(post.filter((pst) => pst.category === categoryId));
+    }, 1000);
+  });
+};
--- src/components/PostContainer/Post.jsx
+++ src/components/PostContainer/Post.jsx
@@ -1,4 +1,4 @@
-const Post = ({titulo, info, img, titulo2, info2a, info2b, info2c, titulo3, info3a, info3b, info3c, titulo4, info4a, info4b, info4c}) => {
+const Post = ({titulo, info, img, titulo2, info2a, info2b, info2c, titulo3, info3a, info3b, info3c, info3d, info3e, titulo4, info4a, info4b, info4c}) => {
     return (
         <div className="infoPost">
             <br />
@@ -6,24 +6,27 @@ const Post = ({titulo, info, img, titulo2, info2a, info2b, info2c, titulo3, info
             <br />
             <h6>{info}</h6>
             <img className="img-fluid" src={img} alt="" />
-            <h3>{titulo2}</h3>
+            <h4>{titulo2}</h4>
             <ul>
             <li>{info2a}</li>
             <li>{info2b}</li>
             <li>{info2c}</li>
             </ul>
-            <h3>{titulo3}</h3>
+            <h4>{titulo3}</h4>
             <ul>
             <li>{info3a}</li>
             <li>{info3b}</li>
             <li>{info3c}</li>
+            <li>{info3d}</li>
+            <li>{info3e}</li>
             </ul>
-            <h3>{titulo4}</h3>
+            <h4>{titulo4}</h4>
             <ul>
             <li>{info4a}</li>
             <li>{info4b}</li>
             <li>{info4c}</li>
             </ul>
+            <button>Reserva ahora!</button>
         </div>
     )
 }
